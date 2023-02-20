7ca0b2487909ce575fbb8ac7ad32cc0d2c3c7764
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-02-20 14:27:22 +0100

    chapter 1 exercise Movie

---
+++ Net development/www/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get:/HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        
+        //Get:/HelloWorld/Welcome
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+            //return $"Hello {name},id is:{id}";
+            //return "This is the welcome action method...";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Net development/www/RentalBike/BikeRental.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalBike
+{
+   
+    internal class BikeRental
+    {
+        List<T> OverviewRentals = new List<T>();
+        
+    }
+    class BikeRental : Material { 
+    public void RegisterRent
+    {
+        if (OverviewRentals.Contains(Serialnumber)==true){
+            Console.WriteLine("Item exists!");
+        }
+            
+        else{
+            OverviewRentals.Add(new BikeRental());
+        }
+    public void DeregisterRent
+    {
+        OverviewRentals.Remove();
+    }
+}
+
+}
