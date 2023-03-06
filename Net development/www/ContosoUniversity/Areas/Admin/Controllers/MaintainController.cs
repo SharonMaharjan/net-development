8a9d4871b3e5a9f1dff11401dc4e3c6c450d56f3
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-03-06 16:10:05 +0100

    adding the extract folders

--- Net development/www/ContosoUniversity/Areas/Admin/Controllers/MaintainController.cs
+++ Net development/www/ContosoUniversity/Areas/Admin/Controllers/MaintainController.cs
@@ -1,16 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoUniversity.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize(Roles = "Administrator")]
-    public class MaintainController:Controller
+    public class MaintainController : Controller
     {
+        [Area("Admin")]
         public IActionResult Index()
         {
             return View();
         }
-
     }
 }
