bc711c7cc9284fedf4d4148c4061fcfa892ddec5
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-03-12 21:21:16 +0100

    working on exercise Moto gp part 1

---
+++ Net development/www/MotoGP/Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotoGP.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OrderTicket()
+        {
+            return View();
+        }
+        public IActionResult ConfirmOrder()
+        {
+            //var banner3 =  
+            return View();
+        }
+    }
+}
