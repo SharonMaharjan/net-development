afcfed69c429ad893e90ad9728df38434f225ac3
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-03-12 22:02:15 +0100

    working on MP project

--- Net development/www/MotoGP/Controllers/InfoController.cs
+++ Net development/www/MotoGP/Controllers/InfoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MotoGP.Models;
 
 namespace MotoGP.Controllers
 {
@@ -7,6 +8,22 @@ namespace MotoGP.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ListRaces()
+        {
+            return View();
+        }
+
+        public IActionResult BuildMap()
+        {
+            var races = new List<Race>() {
+            new Race(){ RaceID = 1, X = 517, Y = 19, Name = "Assen" },
+            new Race(){ RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" },
+            new Race(){ RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" }
+            };
+            return View();
+
         }
     }
 }
