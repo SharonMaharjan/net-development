4550b4063f05cd857ccff8c56d2b69b055c229fa
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-03-28 18:59:38 +0200

    completed doing the  dependency infection exercise

--- Net development/www/DI_Pattern - start/DI_Pattern - start/DI_Pattern_Better/Program.cs
+++ Net development/www/DI_Pattern - start/DI_Pattern - start/DI_Pattern_Better/Program.cs
@@ -7,9 +7,10 @@ IServiceProvider serviceProvider;
 var services = new ServiceCollection();
 // Register interfaces using factory that returns implementation
 services.AddSingleton<IContact, EmailSender>();
+services.AddSingleton<IContact, TelSender>();
 serviceProvider = services.BuildServiceProvider(true);
 
-Order order = new Order(serviceProvider.GetService<IContact>());
+Order order = new Order(serviceProvider.GetServices<IContact>().ElementAt(0));
 order.ContactCustomer(1, "Finally shipped");
 
 Console.WriteLine("Press any key");
---
+++ Net development/www/ExerciseDI_FeedReader - start/ExerciseDI_FeedReader/FeedService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDI_FeedReader
+{
+    public class FeedService
+    {
+
+
+        private readonly IFeedService _service;
+
+        public FeedService(IFeedService service)
+        {
+            _service = service;
+        }
+
+        public string GetFeed()
+        {
+             return _service.GetSingleFeed() ;      
+        }
+    }
+}
