1cde8d05bdd26cc2ece7f5f0ea0fa63cc28d46ee
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-04-17 16:41:09 +0200

    done working with the repository exercise

---
+++ Net development/www/MvcMovieDemo - start part 2/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcMovie.DAL;
+//using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models.ViewModels;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+	public class MoviesController : Controller
+	{
+		private IRepository<Movie> movieRepository;
+		private IRepository<Rating> ratingRepository;
+
+		public MoviesController(IRepository<Movie> movieRepository,IRepository<Rating> ratingRepository)
+		{
+			this.movieRepository = movieRepository;
+			this.ratingRepository = ratingRepository;
+
+            
+		}
+
+		//public MoviesController(MovieContext context)
+		//{
+  //          movieRepository = new GenericRepository<Movie>(context);
+		//	ratingRepository = new GenericRepository<Rating>(context);
+
+		//}
+
+		public IActionResult List(int ratingID = 0)
+		{
+			var listMoviesVM = new ListMoviesViewModel();
+
+			if (ratingID != 0)
+			{
+			listMoviesVM.Movies = movieRepository.Get(
+				filter: m => m.RatingID == ratingID,
+				orderBy: m => m.OrderBy(m => m.Title)).ToList();
+					
+			
+			}
+			else
+			{
+				listMoviesVM.Movies = movieRepository.Get(
+					orderBy: m=>m.OrderBy(x => x.Title)).ToList();
+			}
+
+			listMoviesVM.Ratings =
+				new SelectList(ratingRepository.Get(
+					
+					orderBy: m=>m.OrderBy(x => x.Name)).ToList(),
+					"RatingID", "Name");
+					
+							
+			listMoviesVM.ratingID = ratingID;
+
+			return View(listMoviesVM);
+		}
+
+		public IActionResult Details(int id)
+		{
+			//var movie = _context.Movies
+							//.Include(m => m.Rating)
+							//.SingleOrDefault(m => m.MovieID == id);
+			var movie = movieRepository.Get(
+				filter: x => x.MovieID == id,
+				includes:x => x.Rating).FirstOrDefault();
+				
+			return View(movie);
+		}
+
+		// GET: Movies/Create
+		public IActionResult Create()
+		{
+			ViewData["Ratings"] =
+				new SelectList(ratingRepository.GetAll().OrderBy(r => r.Name),
+							   "RatingID",
+							   "Name");
+
+			return View();
+		}
+
+		// POST: Movies/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create([Bind("RatingID,Name")] Rating rating)
+		{
+			if (ModelState.IsValid)
+			{
+				ratingRepository.Insert(rating);
+				ratingRepository.Save();
+				return RedirectToAction("List");
+			}
+			return View(rating);
+		}
+
+	}
+}
---
+++ Net development/www/MvcMovieDemo - start part 2/MvcMovie/Controllers/RatingsController.cs
@@ -0,0 +1,90 @@
+using System.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+using MvcMovie.DAL;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class RatingsController : Controller
+    {
+        private IRepository<Rating> ratingRepository;
+
+        public RatingsController(MovieContext context)
+        {
+            ratingRepository = new GenericRepository<Rating>(context);    
+        }
+
+        // GET: Ratings/List
+        public IActionResult List()
+        {
+            //ratingRepository.GetAll();
+            //var ratings = ratingRepository.Ratings.OrderBy(r => r.Name);
+            //ratingRepository.OrderBy(ratingRepository => ratingRepository.Name);
+
+            return View(ratingRepository.GetAll());
+        }
+
+        // GET: Ratings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ratings/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("RatingID,Code,Name")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                ratingRepository.Insert(rating);
+                ratingRepository.Save();
+                return RedirectToAction("List");
+            }
+            return View(rating);
+        }
+
+        // GET: Ratings/Edit/5
+        public IActionResult Edit(int id)
+        {
+            //var rating = ratingRepository.GetByID(id);
+
+            //return View(rating);
+            return View(ratingRepository.GetByID(id));
+        }
+
+        // POST: Ratings/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("RatingID,Code,Name")] Rating rating)
+        {
+			if (ModelState.IsValid)
+            {
+                try
+                {
+                  ratingRepository.Update(rating);
+                  ratingRepository.Save();
+                }
+                catch (DataException)
+                {
+                    throw;
+                }
+                return RedirectToAction("List");
+            }
+            return View(rating);
+        }
+
+        // GET: Ratings/Delete/5
+        public IActionResult Delete(int id)
+        {
+            //var rating = _context.Ratings.SingleOrDefault(r => r.RatingID == id);
+            ratingRepository.Delete(id);
+            ratingRepository.Save();
+            return RedirectToAction("List");
+        }
+
+    }
+}
