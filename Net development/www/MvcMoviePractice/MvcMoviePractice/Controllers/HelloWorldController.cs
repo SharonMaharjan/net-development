4c885c08b84d787bb81143169d35f50d98cb3320
sharonmaharjan18 <r0729187@student.thomasmore.be>
2023-06-01 10:49:04 +0200

    Practice for exam

---
+++ Net development/www/MvcMoviePractice/MvcMoviePractice/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMoviePractice.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //get method of hello world
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //get method of hello world welcome
+        public IActionResult Welcome(string name, int numTimes=3)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ Net development/www/MvcMoviePractice/MvcMoviePractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMoviePractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
